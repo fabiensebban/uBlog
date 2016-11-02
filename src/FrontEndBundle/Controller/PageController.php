feee3a9ddaa8fa6038a99c0603d87383aac059d1
ghansum <ghansumn@gmail.com>
2016-11-02 22:46:11 +0100

    modifications du Front

--- src/FrontEndBundle/Controller/PageController.php
+++ src/FrontEndBundle/Controller/PageController.php
@@ -18,10 +18,15 @@ class PageController extends Controller
      * @Route("/")
      * @Method({"GET"})
      */
+
     public function indexAction()
     {
+        $request = $this->container->get('request');
+        $routeName = $request->get('_route');
+        $categories = $this->getDoctrine()->getRepository("AppBundle:Category")->findAll();
         return $this->render('FrontEndBundle:Page:index.html.twig', array(
-            // ...
+            'categories' => $categories,
+            'routeName' => $routeName
         ));
     }
 
@@ -31,8 +36,11 @@ class PageController extends Controller
      */
     public function aboutAction()
     {
+        $request = $this->container->get('request');
+        $routeName = $request->get('_route');
+
         return $this->render('FrontEndBundle:Page:about.html.twig', array(
-            // ...
+            'routeName' => $routeName
         ));
     }
 
@@ -42,9 +50,25 @@ class PageController extends Controller
      */
     public function contactAction()
     {
+        $request = $this->container->get('request');
+        $routeName = $request->get('_route');
+
         return $this->render('FrontEndBundle:Page:contact.html.twig', array(
-            // ...
+            'routeName' => $routeName
         ));
     }
 
+    /**
+     * @Route("/show_profile")
+     * @Method({"GET"})
+     */
+    public function showprofileAction()
+    {
+        $request = $this->container->get('request');
+        $routeName = $request->get('_route');iiii~H¨
+o
+        return $this->render('FrontEndBundle:Page:showUser.html.twig', array(
+            'routeName' => $routeName
+        ));
+    }
 }
