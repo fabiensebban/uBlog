722081e448d5fcf90c65b9793748612c2e2861eb
uness <youness.mou@gmail.com>
2016-12-02 14:08:16 +0100

    create bo index page

--- src/AppBundle/Repository/PostRepository.php
+++ src/AppBundle/Repository/PostRepository.php
@@ -10,4 +10,27 @@ namespace AppBundle\Repository;
  */
 class PostRepository extends \Doctrine\ORM\EntityRepository
 {
+	public function getUserPosts($userId, $limit = null)
+	{
+		$query = "SELECT p FROM AppBundle:Post p WHERE p.author = ".$userId." ORDER BY p.created DESC";
+		if ($limit) {
+			$query .= " LIMIT ".$limit;
+		}
+
+		return $this->getEntityManager()
+            		->createQuery($query)
+            		->getResult();
+	}
+
+	//get likes & shares count of user
+	public function getUserPostsRecap($userId)
+	{
+		$query = "SELECT SUM(p.likes) AS count_likes, SUM(p.shares) AS count_shares  
+				  FROM AppBundle:Post p 
+				  WHERE p.author = ". $userId;
+		
+		return $this->getEntityManager()
+            		->createQuery($query)
+            		->getSingleResult();
+	}
 }
---
+++ src/BackEndBundle/Controller/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BackEndBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route(
+     *     "/bo",
+     *     name="bo_index"
+     * )
+     * @Method({"GET"})
+     */
+    public function indexAction()
+    {
+    	$userId = 1;
+    	$em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository('AppBundle:Post')->getUserPosts($userId);
+        $recap = $em->getRepository('AppBundle:Post')->getUserPostsRecap($userId);
+        //dump($recap);die();
+        return $this->render('BackEndBundle:Index:index.html.twig', array(
+        	'posts'	=>	$posts,
+        	'likes_count' => $recap['count_likes'],
+        	'shares_count' => $recap['count_shares']
+        ));
+    }
+
+}
