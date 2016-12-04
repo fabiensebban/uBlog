d6da066ca22e26ab0798699375522e7da17d80fd
PatpatP <pol.patrick1411@gmail.com>
2016-12-04 22:01:55 +0100

    Translating to french if user is french. If not, translate to English

---
+++ src/AppBundle/EventListener/LocaleListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LocaleListener implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+
+        //Define french if user's preferred language is french
+        $request->getSession()->set('_locale', strpos($request->getPreferredLanguage(), 'fr') === false ? $this->defaultLocale : 'fr' );
+
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered after the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 15)),
+        );
+    }
+}
